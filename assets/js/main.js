e81df0173e66fbf807965eb2c5ec93292be79423
anisen <ananavelez@gmail.com>
2017-06-29 10:30:08 -0400

    avanzando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-	$("#agregar").click(function(){
-        input= $("#inputt").val();
-        $('#acomulador').append('<p><input class="with-gap" name="group3" type="radio" id="test5" checked /><label for="test5">' + input +'</label></p>' +'<a class="waves-effect waves-light btn" id="remover">remove</a>');
+	$("#agregar").click(function(e){
+		e.preventDefault();
+		input= $("#inputt").val();
+		check= $('input[type=checkbox]').click();
+		//$('#acomulador').append('<p><input class="with-gap" name="group3" type="radio" checked /><label for="test5">' + input +'</label></p>' +'<button class="waves-effect waves-light btn" id="remover">X</button>');
+        $('#acomulador').append(check + input + '<br>')
 
+		$("#remover").click(function(){
+			$(input).remove();
+		});
 	});
-
 });
