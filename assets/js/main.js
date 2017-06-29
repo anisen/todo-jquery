d2cc8a8107a86057dc5022695c5855c146b6b9c3
anisen <ananavelez@gmail.com>
2017-06-29 10:42:06 -0400

    mmmmmmmm

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 		e.preventDefault();
 		input= $("#inputt").val();
 		check= $('input[type=checkbox]').click();
-		//$('#acomulador').append('<p><input class="with-gap" name="group3" type="radio" checked /><label for="test5">' + input +'</label></p>' +'<button class="waves-effect waves-light btn" id="remover">X</button>');
-        $('#acomulador').append(check + input + '<br>')
+		$('#acomulador').append('<p><input class="with-gap" name="group3" type="radio" id="test5" checked /><label for="test5">' + input +'</label></p>' +'<button class="waves-effect waves-light btn" id="remover">X</button>');
+       // $('#acomulador').append(check + input + '<br>')
 
 		$("#remover").click(function(){
 			$(input).remove();
